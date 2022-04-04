a48ab8163d649ea1f63ebb5543135ac4bc1ef07d
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-04 11:10:44 +0530

    Update Test_BMS_Sender.cpp

--- Test_BMS_Sender.cpp
+++ Test_BMS_Sender.cpp
@@ -13,6 +13,10 @@ TEST_CASE("Test for Checking sensed current values within range- Sensor 1 ") {
   ReadBMS_DataFromFile( Temperature,StateOfCharge,ChargeRate);
   result = (Temperature[0]==2 && StateOfCharge[0]==5 && ChargeRate[0]==0.0);
   REQUIRE(result ==1);
-  //REQUIRE(Temperature[25]==56 && StateOfCharge[0]==70 && ChargeRate[0]==0.75); 
- // REQUIRE(Temperature[49]==27 && StateOfCharge[49]==67 && ChargeRate[49]==0.85); 
+  
+  result = (Temperature[25]==56 && StateOfCharge[0]==70 && ChargeRate[0]==0.75);
+  REQUIRE(result ==1);
+  
+  result = (Temperature[49]==27 && StateOfCharge[49]==67 && ChargeRate[49]==0.85);
+  REQUIRE(result ==1);
 }
