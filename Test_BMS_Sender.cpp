9a3408168189ee139f2e4cb17ff07ba6ef298923
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-04 12:10:37 +0530

    Update Test_BMS_Sender.cpp

--- Test_BMS_Sender.cpp
+++ Test_BMS_Sender.cpp
@@ -14,11 +14,11 @@ TEST_CASE("Test for Checking weather read of data from file is successful ") {
   result = (Temperature[0]==2 && StateOfCharge[0]==5 && ChargeRate[0]==0.0);
   REQUIRE(result ==1);
   
-  //result = (Temperature[25]==56 && StateOfCharge[0]==70 && ChargeRate[0]==0.75);
-  //REQUIRE(result ==1);
+  result = (Temperature[25]==float(56) && StateOfCharge[0]==float(70) && ChargeRate[0]==float(0.75));
+  REQUIRE(result ==1);
   printf(" %f   %f   %f\n", Temperature[25],StateOfCharge[25],ChargeRate[25]);
   
-  result = (Temperature[49]==27 && StateOfCharge[49]==67 && ChargeRate[49]==0.85);
+  result = (Temperature[49]==float(27) && StateOfCharge[49]==float(67) && ChargeRate[49]==float(0.85));
   REQUIRE(result ==1);
   printf(" %f   %f   %f\n", Temperature[49],StateOfCharge[49],ChargeRate[49]);
 }
