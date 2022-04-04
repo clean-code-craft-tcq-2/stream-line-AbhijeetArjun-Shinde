995be08b30728ebbb9b4936090bd6ceb71066e37
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-04 12:59:59 +0530

    Update Sender.c

--- Sender/Sender.c
+++ Sender/Sender.c
@@ -17,3 +17,17 @@ void ReadBMS_DataFromFile(float* Temp, float* SOC, float* CR)
     }
     fclose(file);  
 }
+
+void SendBMS_DataToConsole(float* Temp, float* SOC, float* CR)
+{
+    for(int i = 0; i<NO_OF_SAMPLES;i++);
+    {
+     printf(" Temperature: %f deg C , State of Charge: %f , Charge Rate:%f \n",  *(Temp+i),*(SOC+i),*(CR+i));
+    }
+}
+void BMS_Sender()
+{
+  float Temperature[NO_OF_SAMPLES],StateOfCharge[NO_OF_SAMPLES],ChargeRate[NO_OF_SAMPLES] = {0};
+  ReadBMS_DataFromFile( Temperature,StateOfCharge,ChargeRate);
+  SendBMS_DataToConsole( Temperature,StateOfCharge,ChargeRate);
+}
