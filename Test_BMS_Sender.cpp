94478ba4db0d0c0380291262ecfbf1bb1b09dbdd
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-04 11:52:19 +0530

    Update Test_BMS_Sender.cpp

--- Test_BMS_Sender.cpp
+++ Test_BMS_Sender.cpp
@@ -14,9 +14,9 @@ TEST_CASE("Test for Checking weather read of data from file is successful ") {
   result = (Temperature[0]==2 && StateOfCharge[0]==5 && ChargeRate[0]==0.0);
   REQUIRE(result ==1);
   
-  result = (Temperature[25]==56 && StateOfCharge[0]==70 && ChargeRate[0]==0.75);
-  REQUIRE(result ==1);
-  
-  //result = (Temperature[49]==27 && StateOfCharge[49]==67 && ChargeRate[49]==0.85);
+  //result = (Temperature[25]==56 && StateOfCharge[0]==70 && ChargeRate[0]==0.75);
   //REQUIRE(result ==1);
+  
+  result = (Temperature[49]==27 && StateOfCharge[49]==67 && ChargeRate[49]==0.85);
+  REQUIRE(result ==1);
 }
