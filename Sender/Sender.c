e1957563ee726336ab2bef6e4b2d18a44b90dc0e
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-04 10:38:22 +0530

    Update Sender.c

--- Sender/Sender.c
+++ Sender/Sender.c
@@ -1,1 +1,16 @@
+#include "Sender.h"
 
+void ReadBMS_DataFromFile(float* Temp, float* SOC, float* CR)
+{
+    int senderInputValue = 0;
+    FILE * file= fopen("./Sender/Sender.txt","r");  
+    if (file!=NULL) {
+        for(int i=0;fscanf(file, "%f\t%f\t%f\n", &Temp,&SOC,&CR)!=EOF ;i++)
+        {
+            *(Temp+i) = Temp;
+            *(SOC+i)  = SOC;
+            *(CR+i)   = CR;
+        }
+    }
+    fclose(file);  
+}
