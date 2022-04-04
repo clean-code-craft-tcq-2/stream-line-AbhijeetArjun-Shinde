254584321a0eb77ba72b35a8d37dc8785e6a0eea
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-04 12:44:51 +0530

    Update Test_BMS_Sender.cpp

--- Test_BMS_Sender.cpp
+++ Test_BMS_Sender.cpp
@@ -12,8 +12,17 @@ TEST_CASE("Test for Checking weather read of data from file is successful ") {
   float ChargeRate[NO_OF_SAMPLES] = {0};
   ReadBMS_DataFromFile( Temperature,StateOfCharge,ChargeRate);
   
-  result = (Temperature[0]==2 && StateOfCharge[0]==5 && ChargeRate[0]==0.0 && Temperature[49]==float(27) && StateOfCharge[49]==float(67) && ChargeRate[49]==float(0.85));
-  REQUIRE(result ==1);
+  float expectedoutput[3][2] = {{2,5,0.0}, {55,67,0.72},{35,47,0.38}};
+  int j =0;
+  for(int i=0;i<3;i++)
+  {
+    REQUIRE(Temperature[j] == expectedoutput[i,0];
+    REQUIRE(StateOfCharge[j] == expectedoutput[i,1];
+    REQUIRE(ChargeRate[j] == expectedoutput[i,2];
+    j=j+24;
+  }
+  //result = (Temperature[0]==2 && StateOfCharge[0]==5 && ChargeRate[0]==0.0 && Temperature[49]==float(27) && StateOfCharge[49]==float(67) && ChargeRate[49]==float(0.85));
+  //REQUIRE(result ==1);
   
   //result = (Temperature[49]==float(27) && StateOfCharge[49]==float(67) && ChargeRate[49]==float(0.85));
   //REQUIRE(result ==1);
