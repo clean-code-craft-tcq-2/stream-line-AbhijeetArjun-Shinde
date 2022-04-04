50407d07f25f5e37889bfa293866a8f4a737735c
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-04 09:17:55 +0530

    Update Test_BMS_Sender.cpp

--- Sender/Test_BMS_Sender.cpp
+++ Sender/Test_BMS_Sender.cpp
@@ -1,9 +1,9 @@
 // Test driven Development. -> Define the testcase before implementation.
 #define CATCH_CONFIG_MAIN 
 
-#include "test/catch.hpp"
-#include "Sender/Sender.h"
 
+#include "Sender/Sender.h"
+#include "test/catch.hpp"
 
 TEST_CASE("Test for Checking sensed current values within range- Sensor 1 ") {
   bool result=0;
