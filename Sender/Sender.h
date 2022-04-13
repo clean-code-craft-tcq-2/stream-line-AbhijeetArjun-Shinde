30f553c7372477edd90568ed78315244f1dc5189
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-13 08:18:13 +0530

    Update Sender.h

--- Sender/Sender.h
+++ Sender/Sender.h
@@ -1,5 +1,10 @@
 #define NO_OF_SAMPLES 50
 
+typedef struct{
+  float Temperature;
+  float StateOfCharge;
+  float ChargeRate;
+}BatteryParameters;
 
 void ReadBMS_DataFromFile(float* Temp, float* SOC, float* CR);
 void SendBMS_DataToConsole(float* Temp, float* SOC, float* CR);
