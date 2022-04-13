bc824f2d659e6a48b79497093873087086a25513
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-13 12:11:59 +0530

    Update Sender.c

--- Sender/Sender.c
+++ Sender/Sender.c
@@ -34,7 +34,10 @@ void BMS_Sender(FILE *InputFile)
 {
   BatteryParameters B1[NO_OF_SAMPLES];
   FILE *OutputPath = stdout;
+  if (InputFile!=NULL) 
+  {
   ReadBMS_DataFromFile(B1,InputFile);
+  }
   for(int i = 0; i<NO_OF_SAMPLES;i++)
     {
      printf(" {\"Temperaturevv\": %.2f degC, \"StateOfCharge\": %.2f, \"ChargeRate\": %.2f}\n",  B1[i].Temperature,B1[i].StateOfCharge,B1[i].ChargeRate);
