0a1faa3ee167b353625250d1f31ee607411d677c
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-13 11:32:52 +0530

    Update Sender.c

--- Sender/Sender.c
+++ Sender/Sender.c
@@ -6,8 +6,8 @@
 void ReadBMS_DataFromFile(BatteryParameters *BP, FILE *InputFile)
 {
     float Temperature, StateOfCharge,ChargeRate;
-    if (file!=NULL) {
-        for(int i=0;fscanf(file, "%f\t%f\t%f\n", &Temperature,&StateOfCharge,&ChargeRate)!=EOF ;i++)
+    if (InputFile!=NULL) {
+        for(int i=0;fscanf(InputFile, "%f\t%f\t%f\n", &Temperature,&StateOfCharge,&ChargeRate)!=EOF ;i++)
         {
             (BP->Temperature) = Temperature;
             (BP->StateOfCharge)  = StateOfCharge;
@@ -25,7 +25,7 @@ void SendBMS_DataToConsole(BatteryParameters *BP, FILE *OutputFile)
     for(int i = 0; i<NO_OF_SAMPLES;i++)
     {
      sprintf(datastream, " {\"Temperature\": %.2f degC, \"StateOfCharge\": %.2f, \"ChargeRate\": %.2f}",  BP->Temperature,BP->StateOfCharge,BP->ChargeRate);
-     fprintf(file,"%s\n",datastream);
+     fprintf(OutputFile,"%s\n",datastream);
      BP++;
     }
 }
@@ -33,6 +33,6 @@ void BMS_Sender(FILE *InputFile)
 {
   BatteryParameters B1[NO_OF_SAMPLES];
   FILE *OutputPath = stdout;
-  ReadBMS_DataFromFile(B1,file);
+  ReadBMS_DataFromFile(B1,InputFile);
   SendBMS_DataToConsole(B1,OutputPath);
 }
