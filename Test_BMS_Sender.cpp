493c13b0295e601f2dcba101948c7920ace791c5
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-13 08:37:13 +0530

    Update Test_BMS_Sender.cpp

--- Test_BMS_Sender.cpp
+++ Test_BMS_Sender.cpp
@@ -8,15 +8,15 @@
 TEST_CASE("Test for Checking weather read of data from file is successful ") 
 {
   BatteryParameters B2[NO_OF_SAMPLES];
-  ReadBMS_DataFromFile( BP,".Sender/Sender.txt");
+  ReadBMS_DataFromFile( B2,".Sender/Sender.txt");
   
   float expectedoutput[3][3] = {{2,5,0.0}, {55,67,0.72},{35,47,0.38}};
   int j =0;
   for(int i=0;i<3;i++)
   {
-    REQUIRE(Temperature[j] == expectedoutput[i][0]);
-    REQUIRE(StateOfCharge[j] == expectedoutput[i][1]);
-    REQUIRE(ChargeRate[j] == expectedoutput[i][2]);
+    REQUIRE(B2[j]->Temperature == expectedoutput[i][0]);
+    REQUIRE(B2[j]->StateOfCharge == expectedoutput[i][1]);
+    REQUIRE(B2[j]->ChargeRate == expectedoutput[i][2]);
     j=j+24;
   }
   BMS_Sender();
