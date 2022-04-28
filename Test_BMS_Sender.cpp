499b97a1762787f8724a82f876a276c9e3fc892c
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-28 10:58:36 +0530

    Update Test_BMS_Sender.cpp

--- Test_BMS_Sender.cpp
+++ Test_BMS_Sender.cpp
@@ -7,20 +7,23 @@
 
 TEST_CASE("Test for Checking weather read of data from file is successful ") 
 {
-  BatteryParameters B2[NO_OF_SAMPLES];
+  BatteryParameters BMS_Data[NO_OF_SAMPLES];
   FILE * file= fopen("./Sender/Sender.txt","r");
-  ReadBMS_DataFromFile( B2,file);
+  FILE *OutputPath = stdout;
+  
+  
+  //ReadBMS_DataFromFile( BMS_Data,file);
   
   float expectedoutput[3][3] = {{2,5,0.0}, {55,67,0.72},{35,47,0.38}};
   int j =0;
   for(int i=0;i<3;i++)
   {
-    REQUIRE( (B2[j].Temperature) == expectedoutput[i][0]);
-    REQUIRE( (B2[j].StateOfCharge) == expectedoutput[i][1]);
-    REQUIRE( (B2[j].ChargeRate) == expectedoutput[i][2]);
+    REQUIRE( (BMS_Data[j].Temperature) == expectedoutput[i][0]);
+    REQUIRE( (BMS_Data[j].StateOfCharge) == expectedoutput[i][1]);
+    REQUIRE( (BMS_Data[j].ChargeRate) == expectedoutput[i][2]);
     j=j+24;
   }
-  FILE * file2= fopen("./Sender/Sender.txt","r");
-  FILE *OutputPath = stdout;
-  BMS_Sender(file2,OutputPath);
+ // FILE * file2= fopen("./Sender/Sender.txt","r");
+
+  BMS_Sender(BMS_Data,file2,OutputPath);
 }
