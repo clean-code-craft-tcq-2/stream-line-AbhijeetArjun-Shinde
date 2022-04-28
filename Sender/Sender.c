42abe91501f18a8eef2b319743d73a5fd5d7f5c3
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-04-28 10:47:42 +0530

    Update Sender.c

--- Sender/Sender.c
+++ Sender/Sender.c
@@ -3,39 +3,44 @@
 #include <math.h>
 #include <stdlib.h>
 
-void ReadBMS_DataFromFile(BatteryParameters *BP, FILE *InputFile)
+void ReadBMS_DataFromFile(BatteryParameters *BMS_DataPtr, FILE *InputFile)
 {
-    float Temperature, StateOfCharge,ChargeRate;
+    float Temperature_loc, StateOfCharge_loc,ChargeRate_loc;
     if (InputFile!=NULL) {
-        for(int i=0;fscanf(InputFile, "%f\t%f\t%f\n", &Temperature,&StateOfCharge,&ChargeRate)!=EOF ;i++)
+        for(int i=0;fscanf(InputFile, "%f\t%f\t%f\n", &Temperature_loc,&StateOfCharge_loc,&ChargeRate_loc)!=EOF ;i++)
         {
-            (BP->Temperature) = Temperature;
-            (BP->StateOfCharge)  = StateOfCharge;
-            (BP->ChargeRate)   = ChargeRate;
-            BP++;
+            (BMS_DataPtr->Temperature) = Temperature_loc;
+            (BMS_DataPtr->StateOfCharge)  = StateOfCharge_loc;
+            (BMS_DataPtr->ChargeRate)   = ChargeRate_loc;
+            BMS_DataPtr++;
         }
     }
     fclose(InputFile);  
 }
 
-void SendBMS_DataToConsole(BatteryParameters *BP, FILE *OutputFile)
+void SendBMS_DataToConsole(BatteryParameters *BMS_DataPtr, FILE *OutputFile)
 {   
     char buffer[200];
     char *datastream = buffer;
     for(int i = 0; i<NO_OF_SAMPLES;i++)
     {
-     sprintf(datastream, " {\"Temperature\": %.2f degC, \"StateOfCharge\": %.2f, \"ChargeRate\": %.2f}",  (BP->Temperature),(BP->StateOfCharge),(BP->ChargeRate));
+     sprintf(datastream, " {\"Temperature\": %.2f degC, \"StateOfCharge\": %.2f, \"ChargeRate\": %.2f}",  (BMS_DataPtr->Temperature),(BMS_DataPtr->StateOfCharge),(BMS_DataPtr->ChargeRate));
      fprintf(OutputFile,"%s\n",datastream);
-     BP++;
+     BMS_DataPtr++;
     }
 }
-void BMS_Sender(FILE *InputFile)
+void BMS_Sender(FILE *InputFile , FILE *OutputPath )
 {
-  BatteryParameters B1[NO_OF_SAMPLES];
-  FILE *OutputPath = stdout;
+
+  
   if (InputFile!=NULL) 
   {
-  ReadBMS_DataFromFile(B1,InputFile);
+  BatteryParameters InpReadFromFile[NO_OF_SAMPLES];
+  ReadBMS_DataFromFile(InpReadFromFile,InputFile);
   }
-  SendBMS_DataToConsole(B1,OutputPath);
+  if (OutputPath!=NULL) 
+  {
+  SendBMS_DataToConsole(InpReadFromFile,OutputPath);
+  }
+  
 }
